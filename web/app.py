51070e8295dcca0bec579315399c88488e4c61c2
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2018-11-17 15:07:44 +0700

    better template and send text to process

--- web/app.py
+++ web/app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def CekAduan(text):
+    return True
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == "POST":
+        text = request.form['aduan']
+        aduan = []
+        aduan.append(text)
+        aduan.append('ya' if CekAduan(text) else 'bukan')
+        aduans = []
+        aduans.append(aduan)
+        return render_template('index.html', aduans = aduans)
     return render_template('index.html')
